fd57cb75a1dd51d11f310bcc5322811cf822d13b
Headturna <suraj-sharma@live.com>
2018-06-03 14:13:28 +0200

    First

---
+++ C++ Console State RPG/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+//Main function
+//Type Name In-parameters (FUNCTION HEADER)
+int main()
+{
+	//FUNCTION BODY
+
+	//Game loop
+	int i = 1; //Variable
+	int a = 23;
+
+	double d = 1.5;
+	float f = 1.243243;
+	char c = 97;
+
+	std::cout << "Hello World!" << " " << i <<  " " << d << " " << f << " " << c << " " << "\n";
+
+	system("PAUSE");
+
+	return 0; //Exit program
+}
